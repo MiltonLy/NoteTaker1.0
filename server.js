bb1afc7c779eb9a1cdd91b921947508360a763f9
Milton Ly <ly.milton@gmail.com>
2023-01-30 16:17:20 -0800

    notes are updating in db.json but not on the page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const path = require('express')
+const path = require('path')
 const PORT = 3000
 
 const fs = require('fs')
@@ -17,11 +17,53 @@ app.get('/app/notes', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+
+function makeNewNote(body, notesArray) {
+    const notes = body;
+    if (!Array.isArray(notesArray))
+        notesArray = []
+
+    if (notesArray === 0)
+        notesArray.push(0)
+
+    body.id = notesArray[0]
+    notesArray[0]++
+
+    notesArray.push(notes);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesArray, null, 2))
+
+    return notes;
+}
+
+app.post('/api/notes', (req, res) => {
+    const generateNote = makeNewNote(req.body, notes);
+    res.json(generateNote);
+})
+
+function deleteNote(id, notesArray) {
+    for (i = 0; i < notesArray.length; i++) {
+        let noteBlock = notesArray[i]
+
+        if (noteBlock.id == id) {
+            notesArray.splice(i, 1)
+            fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesArray, null, 2))
+        }
+    }
+
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true)
 })
 
 app.listen(PORT, () => {
