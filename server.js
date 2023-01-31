a0326c0f9dff74b3308ae9ff9889bb361bcbface
Milton Ly <ly.milton@gmail.com>
2023-01-30 16:55:49 -0800

    fixed port issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = 3000
+const PORT = porcess.env.PORT || 3000
 
 const fs = require('fs')
 const notes = require('./db/db.json')
